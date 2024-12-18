180b4b946c5e57e1afa874840a8c67b7621278e9
Aqila Zafira <hartaticpg@gmail.com>
2024-12-19 03:27:34 +0700

    admin user

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+
+@app.route('/admin')
+def dashboardpy():
     return render_template('index.html')
 
 @app.route('/about')
@@ -13,6 +14,10 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
